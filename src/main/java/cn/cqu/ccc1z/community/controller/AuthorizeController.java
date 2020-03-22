c7317e941507333b729fbb04f502aa7a15d315b3
Ccc1Z <caiwentao5@qq.com>
2020-03-22 17:18:13 +0800

    log session and cookie

--- src/main/java/cn/cqu/ccc1z/community/controller/AuthorizeController.java
+++ src/main/java/cn/cqu/ccc1z/community/controller/AuthorizeController.java
@@ -9,6 +9,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.servlet.http.HttpServletRequest;
+
 /**
  * Create by Ccc1Z on 2020/03/22
  */
@@ -28,7 +30,8 @@ public class AuthorizeController {
 
     @GetMapping("/callback")
     public String callback(@RequestParam(name = "code") String code,
-                           @RequestParam(name = "state") String state) {
+                           @RequestParam(name = "state") String state,
+                           HttpServletRequest request) {
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
         accessTokenDTO.setCode(code);
         accessTokenDTO.setRedirect_uri(redirectUri);
@@ -37,8 +40,16 @@ public class AuthorizeController {
         accessTokenDTO.setClient_secret(clientSecret);
         String accessToken = githubProvider.getAccessToken(accessTokenDTO);
         GithubUser user = githubProvider.getUser(accessToken);
-        System.out.println(user.getName()+" "+user.getId());
+        //System.out.println(user.getName()+" "+user.getId());
+        if(user != null){
+            //登录成功，写cookie和session
+            request.getSession().setAttribute("user",user);//相当于银行账户创建成功
+            return "redirect:/";
+
+        }else{
+            //登录失败，重新登录
+            return "redirect:/";
+        }
         //登录成果后返回主页
-        return "index";
     }
 }
